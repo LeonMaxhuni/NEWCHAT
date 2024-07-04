8a12042949a32b2c688eaf22fd5ebabaa6acf808
Leon <leon.maxhunii@gmail.com>
2024-07-04 23:16:01 +0200

    fixed

--- frontend/src/components/MessagesList.vue
+++ frontend/src/components/MessagesList.vue
@@ -18,16 +18,23 @@ const data = ref([
     },
 ]);
 
-onMounted(() => {
-    axios.get("http://localhost:8000/users/3/ticket/2")
-    .then(response => {data.value = response.data; console.log(response)})
-    .catch(error => {console.error("There was an error fetching the data:", error);});
+onMounted(async () => {
+    // axios.get("http://localhost:8000/users/3/ticket/2")
+    // .then(response => {data.value = response.data; console.log(response)})
+    // .catch(error => {console.error("There was an error fetching the data:", error);});
+    try {
+        data.value = (await axios.get("http://localhost:8000/users/3/ticket/2")).data;
+        console.log(data.value);
+    }
+    catch (error) {
+        console.log(error);
+    }
 });
 
 </script>
 
 <template>
     <div>
-        <IconMessage v-for="item in data" v-bind:key="item.id" v-bind:message="item.message" v-bind:user_id="item.user_id"/>
+        <IconMessage v-for="item of data" v-bind:key="item.id" v-bind:message="item.message" v-bind:user_id="item.user_id"/>
     </div>
 </template>
